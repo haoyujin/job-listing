85ca42331fe8e10e64d273b2ec145b843b684bdc
haoyujin <haoyujin2012@gmail.com>
2017-01-13 20:18:20 +0800

    step7 upload resume rollback1st

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
-  root 'jobs#index'
-  namespace :admin do
-    resources :jobs
-  end
   namespace :admin do
     resources :jobs do
       member do
@@ -14,5 +9,10 @@ Rails.application.routes.draw do
       end
     end
   end
+  
+  resources :jobs do
+    resources :resumes
+  end
 
+  root 'jobs#index'
 end
