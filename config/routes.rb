be1b7fde57585d88928281c83b7ebea65024a2d9
haoyujin <haoyujin2012@gmail.com>
2017-01-13 22:54:19 +0800

    step8.step2

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,18 @@ Rails.application.routes.draw do
       end
     end
   end
-  
+
+  namespace :admin do
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+
+      resources :resumes
+    end
+  end
+
   resources :jobs do
     resources :resumes
   end
